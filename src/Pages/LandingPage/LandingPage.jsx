663856dee0f055d673b904ff3a5ff38e85ffc086
Victor <victorfernandes1669@gmail.com>
2025-11-11 21:02:57 -0300

    fix: Correção em importações

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import Header from "../../Components/LandingHeader/LandingHeader";
 
-import Section1 from "/src/components/LandingSections/Section1";
-import Section2 from "/src/components/LandingSections/Section2";
+import Section1 from "../../Components/LandingSections/Section1";
+import Section2 from "../../Components/LandingSections/Section2";
 import Section3 from "../../Components/LandingSections/Section3";
 
 import styles from "./LandingPage.module.css";
